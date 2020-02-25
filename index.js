fb0434d2c5a72ecf94f31face9580c8c6c2afda5
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-25 14:29:42 -0600

    brought in sessions and setup

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const authRouter = require('./auth/authRouter');
 
 const {PrivateRoute} = require('./utils');
 
+const session = require('express-session');
+const knexStore = require('connect-session-knex')(session);
+
 const server = express();
+
+const sessionConfig = {
+    name: 'session',
+    secret: 'shh secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge = 1000 * 60 * 10,
+        secure: false,
+        httpOnly: true
+    },
+    store: new knexStore({
+        knex,
+        tablename: 'sessions',
+        createTable: true,
+        sidfieldname: 'sid',
+        clearInterval: 1000 * 60 * 12
+    })
+}
 
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api', authRouter);
 server.use('/api/restricted', PrivateRoute, userRouter);
